a0b871c34b25c67663ec26a03c856b400d13f4ff
Lei Qiao <qiaol618@gmail.com>
2015-10-13 11:24:48 -0500

    signal test for p::d::Tria

--- testOne.cc
+++ testOne.cc
@@ -1,6 +1,6 @@
 // ---------------------------------------------------------------------
 //
-// Copyright (C) 2009 - 2014 by the deal.II authors
+// Copyright (C) 2015 by the deal.II authors
 //
 // This file is part of the deal.II library.
 //
@@ -14,27 +14,241 @@
 // ---------------------------------------------------------------------
 
 
+// test signals of Triangulation class in parallel
+
 #include "../tests.h"
+#include <deal.II/grid/tria.h>
+#include <deal.II/grid/grid_generator.h>
+#include <deal.II/distributed/tria.h>
 
-#include <fstream>
-#include <ostream>
+int signal_counter_create = 0;
+int signal_counter_pre_refinement = 0;
+int signal_counter_post_refinement = 0;
+int signal_counter_pre_coarsening_on_cell = 0;
+int signal_counter_post_refinement_on_cell = 0;
+int signal_counter_copy = 0;
+int signal_counter_clear = 0;
+int signal_counter_any_change = 0;
 
-template<int dim>
+std::ofstream logfile;
 
+void f_create ()
+{
+  ++signal_counter_create;
+  return;
+}
+
+void f_pre_refinement ()
+{
+  ++signal_counter_pre_refinement;
+  return;
+}
+
+void f_post_refinement ()
+{
+  ++signal_counter_post_refinement;
+  return;
+}
+
+template<int dim, int spacedim>
+void f_pre_coarsening_on_cell (const typename Triangulation<dim, spacedim>::cell_iterator &)
+{
+  ++signal_counter_pre_coarsening_on_cell;
+  return;
+}
+
+template<int dim, int spacedim>
+void f_post_refinement_on_cell (const typename Triangulation<dim, spacedim>::cell_iterator &)
+{
+  ++signal_counter_post_refinement_on_cell;
+  return;
+}
+
+template<int dim, int spacedim>
+void f_copy (const Triangulation<dim, spacedim> &)
+{
+  ++signal_counter_copy;
+  return;
+}
+
+void f_clear ()
+{
+  ++signal_counter_clear;
+  return;
+}
+
+void f_any_change ()
+{
+  ++signal_counter_any_change;
+  return;
+}
+
+void serial_output(std::ostringstream &log_string)
+{
+  // Output test log. To get an ordered output, collect all contents to
+  // master node then write out.
+
+  const unsigned int myid (Utilities::MPI::this_mpi_process (MPI_COMM_WORLD));
+  const unsigned int master_id(0);
+  const unsigned int n_slots_total (Utilities::MPI::n_mpi_processes (MPI_COMM_WORLD));
+
+  if (myid == master_id)
+    {
+      logfile << log_string.str();
+    }
+
+  for (unsigned int i_slot=1; i_slot < n_slots_total; ++i_slot)
+    {
+      unsigned int string_length(0);
+      MPI_Status status_dummy;
+
+      if (myid == master_id)
+        {
+          MPI_Recv(&string_length, 1, MPI_UNSIGNED, i_slot, i_slot,
+                   MPI_COMM_WORLD, &status_dummy);
+        }
+      if (i_slot == myid)
+        {
+          string_length = log_string.str().size();
+          MPI_Send(&string_length, 1, MPI_UNSIGNED, master_id ,myid, MPI_COMM_WORLD);
+        }
+
+      std::string receive_buffer;
+
+      if (myid == master_id)
+        {
+          receive_buffer.resize(string_length);
+          MPI_Recv(&(receive_buffer[0]), string_length, MPI_CHAR, i_slot,
+                   n_slots_total+i_slot, MPI_COMM_WORLD, &status_dummy);
+        }
+      if (i_slot == myid)
+        {
+          MPI_Send(&(log_string.str().at(0)), string_length, MPI_CHAR, master_id,
+                   myid+n_slots_total, MPI_COMM_WORLD);
+        }
+
+      if (myid == master_id)
+        {
+          logfile << receive_buffer;
+        }
+    }
+
+  if (myid == master_id)
+    {
+      logfile << std::endl << std::endl;
+    }
+
+  return;
+}
+
+template<int dim, int spacedim>
+void test()
+{
+  signal_counter_create = 0;
+  signal_counter_pre_refinement = 0;
+  signal_counter_post_refinement = 0;
+  signal_counter_pre_coarsening_on_cell = 0;
+  signal_counter_post_refinement_on_cell = 0;
+  signal_counter_copy = 0;
+  signal_counter_clear = 0;
+  signal_counter_any_change = 0;
+
+  typedef parallel::distributed::Triangulation<dim, spacedim> TriaType;
+  const unsigned int myid (Utilities::MPI::this_mpi_process (MPI_COMM_WORLD));
+
+  std::ostringstream log_string;
+  log_string.clear();
+  const std::string prefix = "DEAL::" +
+                             Utilities::int_to_string (dim, 1) +
+                             "d-" +
+                             Utilities::int_to_string (spacedim, 1) +
+                             "d::slot-" +
+                             Utilities::int_to_string (myid, 2) +
+                             "::";
+
+  TriaType tria(MPI_COMM_WORLD);
+
+  tria.signals.create.connect (&f_create);
+  tria.signals.pre_refinement.connect (&f_pre_refinement);
+  tria.signals.post_refinement.connect (&f_post_refinement);
+  tria.signals.pre_coarsening_on_cell.connect (&f_pre_coarsening_on_cell<dim,spacedim>);
+  tria.signals.post_refinement_on_cell.connect (&f_post_refinement_on_cell<dim,spacedim>);
+  tria.signals.copy.connect (&f_copy<dim,spacedim>);
+  tria.signals.clear.connect (&f_clear);
+  tria.signals.any_change.connect (&f_any_change);
+
+  GridGenerator::hyper_cube(tria);
+
+  // Test signal on copying
+  {
+    TriaType tria_cpoier(MPI_COMM_WORLD);
+    tria_cpoier.copy_triangulation(tria);
+  }
+
+  tria.refine_global(2);
+
+  // Test signal on coarsening
+  {
+    typename TriaType::active_cell_iterator cell = tria.begin_active();
+    const typename TriaType::active_cell_iterator endc = tria.end();
+
+    for (; cell != endc; ++cell)
+      {
+        cell->set_coarsen_flag();
+      }
+    tria.execute_coarsening_and_refinement();
+  }
+
+  tria.clear();
+
+  log_string << prefix << "n_signal_create : " << signal_counter_create << std::endl;
+  log_string << prefix << "n_signal_pre_refinement : " << signal_counter_pre_refinement << std::endl;
+  log_string << prefix << "n_signal_post_refinement : " << signal_counter_post_refinement << std::endl;
+  log_string << prefix << "n_signal_pre_coarsening_on_cell : " << signal_counter_pre_coarsening_on_cell << std::endl;
+  log_string << prefix << "n_signal_post_refinement_on_cell : " << signal_counter_post_refinement_on_cell << std::endl;
+  log_string << prefix << "n_signal_copy : " << signal_counter_copy << std::endl;
+  log_string << prefix << "n_signal_clear : " << signal_counter_clear << std::endl;
+  log_string << prefix << "n_signal_any_change : " << signal_counter_any_change << std::endl;
+
+  serial_output(log_string);
+
+  return;
+}
 
 int main(int argc, char *argv[])
 {
-  // Utilities::MPI::MPI_InitFinalize mpi_initialization (argc, argv, 1);
-  // unsigned int myid = Utilities::MPI::this_mpi_process (MPI_COMM_WORLD);
+  Utilities::MPI::MPI_InitFinalize mpi_initialization (argc, argv, /* int max_num_threads */ 1);
+  const unsigned int myid (Utilities::MPI::this_mpi_process (MPI_COMM_WORLD));
+  const unsigned int master_id(0);
 
-  std::ofstream logfile("output");
-  deallog.attach(logfile);
-  deallog.depth_console(0);
-  deallog.threshold_double(1.e-10);
+  if (myid == master_id)
+    {
+      logfile.open("output");
+    }
 
-  deallog.push("2d");
-  test<2>();
-  deallog.pop();
+  // parallel::distributed::Triangulation<1, spacedim> is not valid.
+  {
+    const int dim = 2;
+    const int spacedim = 2;
+    test<dim,spacedim> ();
+  }
+
+  {
+    const int dim = 2;
+    const int spacedim = 3;
+    test<dim,spacedim> ();
+  }
+
+  {
+    const int dim = 3;
+    const int spacedim = 3;
+    test<dim,spacedim> ();
+  }
+
+  if (myid == master_id)
+    {
+      logfile.close();
+    }
 
   return (0);
 }
