ad562f445b7b11056b42b2b4ad1c9caf0ebe641d
Lei Qiao <qiaol618@gmail.com>
2016-01-25 10:13:17 -0600

    Code done

--- testOne.cc
+++ testOne.cc
@@ -20,7 +20,9 @@
 #include <ostream>
 
 #include <deal.II/base/utilities.h>
-
+#include <deal.II/base/std_cxx11/array.h>
+#include <deal.II/base/point.h>
+#include <boost/container/map.hpp>
 /*
  * Purpose:
  *   1. Combine separated file written by parallel processors;
@@ -31,42 +33,196 @@
 template<int dim>
 class DataProcessor
 {
+private:
+  static const unsigned int n_components             = dim+2;
+  static const unsigned int first_momentum_component = 0;
+  static const unsigned int first_velocity_component = 0;
+  static const unsigned int density_component        = dim;
+  static const unsigned int energy_component         = dim+1;
+  static const unsigned int pressure_component       = dim+1;
+
+  struct DataStruct
+  {
+    Point<dim> position;
+    std_cxx11::array<double, n_components> solution;
+    double velocity_magnitude;
+    double sound_speed;
+    double local_Mach;
+    double pressure_coefficient;
+  };
+
+  typedef boost::container::map<double, DataStruct> DataMap;
+
 public:
-  DataProcessor(const std::string &file_name_prefix_in);
+  DataProcessor();
+
 private:
   void run() const;
 
-  const std::string    &file_name_prefix;
-  const unsigned int    solution_component = dim + 2;
+  const double gas_gamm;
 };
 
 
 int main(int argc, char *argv[])
 {
-  // Utilities::MPI::MPI_InitFinalize mpi_initialization (argc, argv, 1);
-  // unsigned int myid = Utilities::MPI::this_mpi_process (MPI_COMM_WORLD);
-
-  std::ofstream logfile("output");
-  deallog.attach(logfile);
-  deallog.depth_console(0);
-  deallog.threshold_double(1.e-10);
-
-  deallog.push("2d");
-
-  deallog.pop();
-
+  DataProcessor<2> data_processor;
   return (0);
 }
 
 template<int dim>
-DataProcessor<dim>::DataProcessor(const std::string &file_name_prefix_in)
+DataProcessor<dim>::DataProcessor()
   :
-  file_name_prefix(file_name_prefix_in)
-{}
+  gas_gamm(1.4)
+{
+  run();
+}
 
 template<int dim>
 void
 DataProcessor<dim>::run() const
 {
+  // Declare data storing object
+  DataMap data_map;
+  data_map.clear();
+  double Mach = -1.0;
+
+  // Gathering data written by all slots
+  for ( unsigned int i_slot=0; i_slot<10000; ++i_slot)
+    {
+      // Open file
+      const std::string file_name = "boundaryData.slot-"
+                                    + Utilities::int_to_string (i_slot,4)
+                                    + ".raw";
+      std::ifstream file_in (file_name.c_str());
+      if (!file_in)
+        {
+          std::cerr << "There is no file '" << file_name << "'.\n"
+                    << "Exit!" << std::endl;
+          break;
+        }
+      std::cerr << "Processing '" << file_name << "'." << std::endl;
+
+      // Read first line for Mach number
+      {
+        double slot_Mach;
+        file_in >> slot_Mach;
+        AssertThrow (slot_Mach == Mach || Mach < 0.0,
+                     ExcMessage("Referencing Mach number mismatch!"));
+        Mach = slot_Mach;
+      }
+
+      // Go through all lines
+      while (true)
+        {
+          double x_index;
+          DataStruct data_struct;
+
+          for (unsigned int id=0; id<dim; ++id)
+            {
+              file_in >> data_struct.position[id];
+            }
+
+          // Check file status after first reading try to avoid
+          // trouble in ending white line.
+          if (file_in.eof())
+            {
+              break;
+            }
+
+          for (unsigned int ic=0; ic<n_components; ++ic)
+            {
+              file_in >> data_struct.solution[ic];
+            }
+
+          data_struct.velocity_magnitude = 0.0;
+          for (unsigned int ic=first_velocity_component, id=0; id<dim; ++ic, ++id)
+            {
+              data_struct.velocity_magnitude += data_struct.solution[ic] * data_struct.solution[ic];
+            }
+          data_struct.velocity_magnitude = std::sqrt(data_struct.velocity_magnitude);
+
+          data_struct.sound_speed
+            = std::sqrt (gas_gamm *
+                         data_struct.solution[pressure_component] /
+                         data_struct.solution[density_component]);
+          data_struct.local_Mach = data_struct.velocity_magnitude /
+                                   data_struct.sound_speed;
+
+          data_struct.pressure_coefficient =
+            2.0 * (data_struct.solution[pressure_component] - 1.0/gas_gamm) /
+            (Mach * Mach);
+
+          // Order data according to x-coordinate by default.
+          x_index = data_struct.position[0];
+          if (data_struct.position[dim-1] < 0.0)
+            {
+              // Order date circle form TE for foil case.
+              // Z-up configuration in 3D is assumed.
+              x_index = std::copysign(x_index, data_struct.position[dim-1]);
+            }
+
+          // Insert new entry
+          data_map[x_index] = data_struct;
+        }
+      file_in.close();
+    }
+
+  // Write out collected data
+  {
+    std::ofstream file_out ("collectedBoundaryData.txt");
+
+    file_out << "# Mach_{infty} = " << Mach << std::endl;
+
+    {
+      unsigned int column = 0;
+      file_out << "#" << ++column << " x-index    ";
+      file_out << "#" << ++column << " x           ";
+      file_out << "#" << ++column << " y          ";
+      if (dim == 3)
+        {
+          file_out << "#" << ++column << " z          ";
+        }
+      file_out << "#" << ++column << " u          ";
+      file_out << "#" << ++column << " v          ";
+      if (dim == 3)
+        {
+          file_out << "#" << ++column << " w          ";
+        }
+      file_out << "#" << ++column << " rho        ";
+      file_out << "#" << ++column << " p          ";
+      file_out << "#" << ++column << " |velocity| ";
+      file_out << "#" << ++column << " v_sound    ";
+      file_out << "#" << ++column << " Mach       ";
+      file_out << "#" << ++column << " Cp         ";
+      file_out << std::endl;
+    }
+
+    file_out.setf (std::ios::scientific);
+    file_out.precision (6);
+
+    for (typename DataMap::iterator it=data_map.begin(); it!=data_map.end(); ++it)
+      {
+        const unsigned int column_with = 13;
+        file_out << std::setw (column_with) << it->first << ' ';
+        const DataStruct &data_struct = it->second;
+        for (unsigned int id=0; id<dim; ++id)
+          {
+            file_out << std::setw (column_with) << data_struct.position[id] << ' ';
+          }
+        for (unsigned int ic=0; ic<n_components; ++ic)
+          {
+            file_out << std::setw (column_with) << data_struct.solution[ic] << ' ';
+          }
+        file_out
+            << std::setw (column_with) << data_struct.velocity_magnitude <<  ' '
+            << std::setw (column_with) << data_struct.sound_speed <<  ' '
+            << std::setw (column_with) << data_struct.local_Mach <<  ' '
+            << std::setw (column_with) << data_struct.pressure_coefficient <<  ' '
+            << std::endl;
+      }
+
+    file_out.close();
+  }
+
   return;
 }
